5281502eba10aa36486e477a5b5b3b9f4d2cedb4
NobodyOffcal <100217070+NobodyOffcal@users.noreply.github.com>
2023-01-20 16:38:43 +0300

    Add files via upload

---
+++ Api.lua
@@ -0,0 +1,9 @@
+--Simple Api V1.5
+
+return
+{
+    
+    "Status_True",
+    "Current_Version_V5",
+
+}
---
+++ Auth.lua
@@ -0,0 +1,11 @@
+--Simple Auth V1.5
+
+return
+{
+   
+    "NobodyOffcal_Banned_False",
+    "NobodyOffcal_Rank_Administrator",
+    "NobodyOffcal_Key_True",
+    "NobodyOffcal_Hwid_Unknown",
+
+}
