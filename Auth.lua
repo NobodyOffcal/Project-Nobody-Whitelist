5e3e9636f21153f5684a67be50b9adf31bf8bd95
NobodyOffcal <100217070+NobodyOffcal@users.noreply.github.com>
2023-01-25 20:13:51 +0300

    Update Auth.lua

--- Auth.lua
+++ Auth.lua
@@ -3,9 +3,9 @@
 return
 {
    
-    "NobodyOffcal_Banned_False",
-    "NobodyOffcal_Rank_Administrator",
-    "NobodyOffcal_Key_True",
-    "NobodyOffcal_Hwid_Unknown",
+    "bobloxvsnobody_Banned_False",
+    "bobloxvsnobody_Rank_Administrator",
+    "bobloxvsnobody_Key_True",
+    "bobloxvsnobody_Hwid_Unknown",
 
 }
