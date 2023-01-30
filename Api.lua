c9686915cbf1d86377451ae60e565fbfcdf88278
NobodyOffcal <100217070+NobodyOffcal@users.noreply.github.com>
2023-01-30 13:49:45 +0300

    Update Api.lua

--- Api.lua
+++ Api.lua
@@ -1,11 +1,9 @@
---Simple Api V1.5
+--Simple Api V1.6
 
 return
 {
     
-    ["Status"] = "lol",
-    "Current_Version_V5",
-    "True_1d3b3c2c",
-    "False_0f2853a2c",
+    ["Status"] = true,
+    ["Version"] = "V5",
     
 }
