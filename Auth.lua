3ef4e2316a922fa2316b74516763e21ec2220dca
NobodyOffcal <100217070+NobodyOffcal@users.noreply.github.com>
2023-01-30 18:46:59 +0300

    Update Auth.lua

--- Auth.lua
+++ Auth.lua
@@ -9,25 +9,4 @@ return
     ["Key-1"] = true,
     ["Hwid-1"] = "186EE260-1F7A-44F6-A4D7-9E1FBDB52DC3",
    
-    ["User-2"] = "test",
-    ["Banned-2"] = false,
-    ["Rank-2"] = "Administrator",
-    ["Key-2"] = true,
-    ["Hwid-2"] = "Unknown",
-   
-    "val3ndccount_Banned_False",
-    "val3ndccount_Rank_Buyer",
-    "val3ndccount_Key_True",
-    "val3ndccount_Hwid_Unknown",
-     
-    "OGstinker024_Banned_False",
-    "OGstinker024_Rank_Buyer",
-    "OGstinker024_Key_True",
-    "OGstinker024_Hwid_Unknown",
-   
-    "fromcattohugecat0274_Banned_False",
-    "fromcattohugecat0274_Rank_Buyer",
-    "fromcattohugecat0274_Key_True",
-    "fromcattohugecat0274_Hwid_Unknown",
-   
 }
