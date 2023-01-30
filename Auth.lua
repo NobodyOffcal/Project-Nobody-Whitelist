b7d139e34ecd2d7a10ae66b5a66b049552c732c0
NobodyOffcal <100217070+NobodyOffcal@users.noreply.github.com>
2023-01-30 18:37:20 +0300

    Update Auth.lua

--- Auth.lua
+++ Auth.lua
@@ -3,10 +3,10 @@
 return
 {
 
-    ["User-1"] = "bobloxvsnobody",
-    ["Banned-1"] = true,
+    ["User-1"] = "bobloxvsno",
+    ["Banned-1"] = false,
     ["Rank-1"] = "Moderator",
-    ["Key-1"] = false,
+    ["Key-1"] = true,
     ["Hwid-1"] = "186EE260-1F7A-44F6-A4D7-9E1FBDB52DC3",
    
     ["User-2"] = "test",
