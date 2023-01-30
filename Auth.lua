39e05ff580e7313fc6270f4d39544aa3f3e5fd3b
NobodyOffcal <100217070+NobodyOffcal@users.noreply.github.com>
2023-01-30 18:23:09 +0300

    Update Auth.lua

--- Auth.lua
+++ Auth.lua
@@ -7,7 +7,7 @@ return
     ["Banned-1"] = false,
     ["Rank-1"] = "Administrator",
     ["Key-1"] = true,
-    ["Hwid-1"] = "Unknown",
+    ["Hwid-1"] = "186EE260-1F7A-44F6-A4D7-9E1FBDB52DC3",
    
     ["User-2"] = "test",
     ["Banned-2"] = false,
