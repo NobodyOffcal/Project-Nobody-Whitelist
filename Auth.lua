5515af6d8826b8329d19b04794417d5e88dccd96
NobodyOffcal <100217070+NobodyOffcal@users.noreply.github.com>
2023-01-31 16:17:51 +0300

    Update Auth.lua

--- Auth.lua
+++ Auth.lua
@@ -3,10 +3,21 @@
 return
 {
 
-    ["User-1"] = "bobloxvsnobody",
-    ["Banned-1"] = false,
-    ["Rank-1"] = "Administrator",
-    ["Key-1"] = true,
-    ["Hwid-1"] = "186EE260-1F7A-44F6-A4D7-9E1FBDB52DC3",
+    ["User"] = "bobloxvsnobody",
+    ["Banned"] = false,
+    ["Rank"] = "Administrator",
+    ["Key"] = true,
+    ["Hwid"] = "186EE260-1F7A-44F6-A4D7-9E1FBDB52DC3",
    
+}
+
+return
+{
+    
+    ["User"] = "sexbody",
+    ["Banned"] = false,
+    ["Rank"] = "Administrator",
+    ["Key"] = true,
+    ["Hwid"] = "186EE260-1F7A-44F6-A4D7-9E1FBDB52DC3",
+    
 }
