c981525446cc81b7383b263a3a54112f566f3c52
NobodyOffcal <100217070+NobodyOffcal@users.noreply.github.com>
2023-01-31 16:27:30 +0300

    Update Auth.lua

--- Auth.lua
+++ Auth.lua
@@ -8,16 +8,11 @@ return
     ["Rank"] = "Administrator",
     ["Key"] = true,
     ["Hwid"] = "186EE260-1F7A-44F6-A4D7-9E1FBDB52DC3",
-   
-}
-
-return
-{
     
     ["User"] = "sexbody",
     ["Banned"] = false,
     ["Rank"] = "Administrator",
     ["Key"] = true,
     ["Hwid"] = "186EE260-1F7A-44F6-A4D7-9E1FBDB52DC3",
-    
+   
 }
