aaac85e3925643066286ebadb3f10ff3dad67655
NobodyOffcal <100217070+NobodyOffcal@users.noreply.github.com>
2023-02-04 00:10:41 +0300

    Add files via upload

---
+++ Auth.lua
@@ -0,0 +1,12 @@
+--Simple Auth V1.6
+
+return
+{
+
+    ["User-1"] = "bobloxvsnobody",
+    ["Banned-1"] = false,
+    ["Rank-1"] = "Administrator",
+    ["Key-1"] = true,
+    ["Hwid-1"] = "186EE260-1F7A-44F6-A4D7-9E1FBDB52DC3",
+   
+}
